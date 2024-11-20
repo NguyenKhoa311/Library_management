f5ea7cb6d82b6905ca0f48b51cb507c740bb33de
NguyenKhoa311 <23020616@vnu.edu.vn>
2024-11-20 23:46:26 +0700

    refactor ImgurUpload class

--- src/main/java/com/uet/libraryManagement/APIService/ImgurUpload.java
+++ src/main/java/com/uet/libraryManagement/APIService/ImgurUpload.java
@@ -1,70 +1,5 @@
-//package com.uet.libraryManagement.APIService;
-//
-//import com.google.gson.JsonObject;
-//import com.google.gson.JsonParser;
-//
-//import java.io.*;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//
-//public class ImgurUpload {
-//    private static final String CLIENT_ID = "c8327df77b699e2";
-//    private static final String IMGUR_UPLOAD_URL = "https://api.imgur.com/3/image";
-//
-//    public static String uploadImage(File imageFile) throws IOException {
-//        URL url = new URL(IMGUR_UPLOAD_URL);
-//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//        connection.setRequestMethod("POST");
-//        connection.setRequestProperty("Authorization", "Client-ID " + CLIENT_ID);
-//        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-//        connection.setDoOutput(true);
-//
-//        // Đọc file ảnh và encode base64
-//        String base64Image = encodeFileToBase64(imageFile);
-//        String data = "image=" + base64Image;
-//
-//        // Gửi dữ liệu
-//        try (OutputStream os = connection.getOutputStream()) {
-//            os.write(data.getBytes());
-//            os.flush();
-//        }
-//
-//        // Đọc phản hồi
-//        int responseCode = connection.getResponseCode();
-//        if (responseCode == HttpURLConnection.HTTP_OK) {
-//            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-//                StringBuilder response = new StringBuilder();
-//                String line;
-//                while ((line = br.readLine()) != null) {
-//                    response.append(line);
-//                }
-//                JsonObject jsonObject = JsonParser.parseString(response.toString()).getAsJsonObject();
-//                if (jsonObject.get("success").getAsBoolean()) {
-//                    return jsonObject.getAsJsonObject("data").get("link").getAsString();
-//                } else {
-//                    throw new IOException("Imgur upload failed: " + jsonObject);
-//                }
-//            }
-//        } else {
-//            throw new IOException("HTTP Error: " + responseCode);
-//        }
-//    }
-//
-//    private static String encodeFileToBase64(File file) throws IOException {
-//        try (FileInputStream fis = new FileInputStream(file);
-//             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-//            byte[] buffer = new byte[1024];
-//            int bytesRead;
-//            while ((bytesRead = fis.read(buffer)) != -1) {
-//                out.write(buffer, 0, bytesRead);
-//            }
-//            return java.util.Base64.getEncoder().encodeToString(out.toByteArray());
-//        }
-//    }
-//}
 package com.uet.libraryManagement.APIService;
 
-import javafx.scene.image.Image;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -72,8 +7,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Base64;
+
 
 public class ImgurUpload {
 
